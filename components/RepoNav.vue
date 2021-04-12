e2bc595715f50d4fd4158da9ca9dfce18dadc2de
Percy Astocaza <percy0x@gmail.com>
2021-04-11 20:45:21 -0500

    :zap: Refactor to components

---
+++ components/RepoNav.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class='border-b border-gray-300 flex items-center px-8 mt-4'>
+    <li class='font-semibold'>
+      <a href='#' class='flex items-center border-b-2 border-red-500 px-4 pb-3'>
+        <svg class='w-4 fill-current' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z'></path>
+        </svg>
+        <span class='ml-2'>Code</span></a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z'></path>
+        </svg>
+        <span class='ml-2'>Issues</span>
+        <div class='text-xs rounded-lg bg-gray-200 px-2 ml-1'>84</div>
+      </a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z'></path>
+        </svg>
+        <span class='ml-2'>Pull requests</span>
+        <div class='text-xs rounded-lg bg-gray-200 px-2 ml-1'>32</div>
+      </a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M1.5 2.75a.25.25 0 01.25-.25h8.5a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-3.5a.75.75 0 00-.53.22L3.5 11.44V9.25a.75.75 0 00-.75-.75h-1a.25.25 0 01-.25-.25v-5.5zM1.75 1A1.75 1.75 0 000 2.75v5.5C0 9.216.784 10 1.75 10H2v1.543a1.457 1.457 0 002.487 1.03L7.061 10h3.189A1.75 1.75 0 0012 8.25v-5.5A1.75 1.75 0 0010.25 1h-8.5zM14.5 4.75a.25.25 0 00-.25-.25h-.5a.75.75 0 110-1.5h.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0114.25 12H14v1.543a1.457 1.457 0 01-2.487 1.03L9.22 12.28a.75.75 0 111.06-1.06l2.22 2.22v-2.19a.75.75 0 01.75-.75h1a.25.25 0 00.25-.25v-5.5z'></path>
+        </svg>
+        <span class='ml-2'>Discussions</span></a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM6.379 5.227A.25.25 0 006 5.442v5.117a.25.25 0 00.379.214l4.264-2.559a.25.25 0 000-.428L6.379 5.227z'></path>
+        </svg>
+        <span class='ml-2'>Actions</span></a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M7.467.133a1.75 1.75 0 011.066 0l5.25 1.68A1.75 1.75 0 0115 3.48V7c0 1.566-.32 3.182-1.303 4.682-.983 1.498-2.585 2.813-5.032 3.855a1.7 1.7 0 01-1.33 0c-2.447-1.042-4.049-2.357-5.032-3.855C1.32 10.182 1 8.566 1 7V3.48a1.75 1.75 0 011.217-1.667l5.25-1.68zm.61 1.429a.25.25 0 00-.153 0l-5.25 1.68a.25.25 0 00-.174.238V7c0 1.358.275 2.666 1.057 3.86.784 1.194 2.121 2.34 4.366 3.297a.2.2 0 00.154 0c2.245-.956 3.582-2.104 4.366-3.298C13.225 9.666 13.5 8.36 13.5 7V3.48a.25.25 0 00-.174-.237l-5.25-1.68zM9 10.5a1 1 0 11-2 0 1 1 0 012 0zm-.25-5.75a.75.75 0 10-1.5 0v3a.75.75 0 001.5 0v-3z'></path>
+        </svg>
+        <span class='ml-2'>Security</span></a>
+    </li>
+    <li>
+      <a href='#'
+         class='flex items-center border-b-2 border-transparent hover:border-gray-300 transition ease-in-out duration-150 px-4 pb-3'>
+        <svg class='w-4 fill-current text-gray-400' viewBox='0 0 16 16' aria-hidden='true'>
+          <path fill-rule='evenodd'
+                d='M1.5 1.75a.75.75 0 00-1.5 0v12.5c0 .414.336.75.75.75h14.5a.75.75 0 000-1.5H1.5V1.75zm14.28 2.53a.75.75 0 00-1.06-1.06L10 7.94 7.53 5.47a.75.75 0 00-1.06 0L3.22 8.72a.75.75 0 001.06 1.06L7 7.06l2.47 2.47a.75.75 0 001.06 0l5.25-5.25z'></path>
+        </svg>
+        <span class='ml-2'>Insights</span></a>
+    </li>
+  </ul>
+</template>
